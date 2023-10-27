835e950007c4aaa6b1121e684c9db1babc7fc405
RamunnoAJ <ramunnoagustin@gmail.com>
2023-10-27 11:43:25 -0300

    add: render of the Memotest

--- src/components/pages/Memotest.js
+++ src/components/pages/Memotest.js
@@ -38,3 +38,36 @@ const useMemotestGameState = () => {
 
   return { tiles, shuffleTiles, gameEnded };
 };
+
+const Memotest = () => {
+  const { tiles, shuffleTiles } = useMemotestGameState();
+
+  useEffect(() => {
+    shuffleTiles();
+  }, []);
+
+  return (
+    <div className="memotest-board">
+      <div className="memotest-row">
+        <Square color={tiles[0]} />
+        <Square color={tiles[1]} />
+        <Square color={tiles[2]} />
+        <Square color={tiles[3]} />
+      </div>
+      <div className="memotest-row">
+        <Square color={tiles[4]} />
+        <Square color={tiles[5]} />
+        <Square color={tiles[6]} />
+        <Square color={tiles[7]} />
+      </div>
+      <div className="memotest-row">
+        <Square color={tiles[8]} />
+        <Square color={tiles[9]} />
+        <Square color={tiles[10]} />
+        <Square color={tiles[11]} />
+      </div>
+      <div className="" onClick={shuffleTiles}>Reiniciar juego</div>
+    </div>
+  );
+};
+export default Memotest;
